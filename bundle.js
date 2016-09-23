f957a7edbfc626a5415808011ce9caadc221ecf2
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-23 16:45:01 -0700

    Now webpack will build a map file, so we can debug.

--- bundle.js
+++ bundle.js
@@ -1,65 +1,71 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/*!****************!*\
+  !*** ./app.js ***!
+  \****************/
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-
-	var _module2 = __webpack_require__(1);
-
+	
+	var _module2 = __webpack_require__(/*! ./module.js */ 1);
+	
 	var _module3 = _interopRequireDefault(_module2);
-
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+	
 	console.log('hello ' + _module3.default.name + '!');
 
 /***/ },
 /* 1 */
+/*!*******************!*\
+  !*** ./module.js ***!
+  \*******************/
 /***/ function(module, exports) {
 
 	'use strict';
-
+	
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
@@ -69,3 +75,4 @@
 
 /***/ }
 /******/ ]);
+//# sourceMappingURL=bundle.js.map
