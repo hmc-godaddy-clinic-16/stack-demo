561933c81e81c533026e13448754f445accd4b99
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-23 16:39:56 -0700

    Connecting babel and web pack

--- bundle.js
+++ bundle.js
@@ -44,17 +44,21 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var module = __webpack_require__(1);
+	'use strict';
 
-	console.log('hello ' + module.name + '!');
+	var _module = __webpack_require__(1);
+
+	console.log('hello ' + _module.name + '!');
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
+	'use strict';
+
 	module.exports = {
 	  name: 'HMU :)'
-	}
+	};
 
 /***/ }
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,13 @@ module.exports = {
   entry: "./app.js",
   output: {
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: "babel-loader"
+      }
+    ]
   }
 }
