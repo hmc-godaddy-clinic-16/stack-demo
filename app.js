6c2f197b12b0a1363212424f26858d5c0731f6f0
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-23 17:32:38 -0700

    More complex components

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HMU from "./module.js";
 
-ReactDOM.render(<HMU name="GoDaddy"/>, document.getElementById("app"));
+ReactDOM.render(<HMU name={["HMU", "GoDaddy"]} special={true} />, document.getElementById("app"));
--- module.js
+++ module.js
@@ -2,7 +2,19 @@ import React from "react";
 
 class HMU extends React.Component {
   render() {
-    return <div>Hello {this.props.name}!</div>;
+    let decorator = "";
+    if (this.props.special) {
+        decorator = "!!!";
+    }
+    return (
+      <div> 
+      {
+        this.props.name.map( (name) => {
+          return <div>Hello {name}{decorator}</div>
+        })
+      }
+      </div>
+    );
   }
 }
 
