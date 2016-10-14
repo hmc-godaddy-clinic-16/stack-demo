294226cd2cc57c3356e2c461b24a2c4d84b427f8
Daniel Gwozdz <dangw@godaddy.com>
2016-10-14 11:42:04 -0700

    Added integration with the redux devtools chrome extension

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const store = createStore(combineReducers({
   friends
 }), {
   friends: ["HMU", "GoDaddy"]
-});
+},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
